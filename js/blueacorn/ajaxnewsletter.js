0eb815026015c022db4ff868cfb683688f1e184d
Grant Wimmer <grant.wimmer@blueacorn.com>
2015-06-15 08:50:08 -0400

    Test adjustment

--- js/blueacorn/ajaxnewsletter.js
+++ js/blueacorn/ajaxnewsletter.js
@@ -1,1 +1,12 @@
 alert("It works! The dream is alive!");
+
+function subscribeToNewsletter(){
+    new Ajax.Request('../test.html', {
+            method: 'get',
+            parameters: $('newsletter-validate-detail').serialize(true),
+            onSuccess: console.log(parameters);
+
+        }
+    )
+
+}
