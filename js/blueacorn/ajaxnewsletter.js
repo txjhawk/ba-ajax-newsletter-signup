79f402a4838f540f5c58300028ee9ee172a5c0b2
Grant Wimmer <grant.wimmer@blueacorn.com>
2015-06-15 18:17:11 -0400

    Added wrapper for newsletter form submission.

--- js/blueacorn/ajaxnewsletter.js
+++ js/blueacorn/ajaxnewsletter.js
@@ -1,21 +1,41 @@
-VarienForm.prototype.submit = VarienForm.prototype.submit.wrap
+// this: VarienForm('newsletter-validate-detail')
+// callOriginal: Original function Validation.prototype.onSubmit
+// ev: form submission event
+
+Validation.prototype.onSubmit = Validation.prototype.onSubmit.wrap
 (
-    function(){
-        console.log("That's a wrap!");
-        return false;
+    function(callOriginal,ev) {
+        if (!this.validate())
+        {
+            // Continue executing the original function (and pass back the event itself) so the user will be notified of the validation error
+            return callOriginal(ev);
+        }
+        else
+        {
+            // Stop the form from processing normally so that we may issue an Ajax call for email submission and simply display a message instead.
+            Event.stop(ev);
+            console.log(this);
+            var email_addr = this.form[0].value;
+
+            new Ajax.Request(this.form.action, {
+                method: 'GET',
+                parameters: {addr: email_addr},
+                onSuccess: function(transport) {
+                    var request_status = transport.status;
+                    console.log(transport.status);
+                    if (status === 200)
+                    {
+                        alert("The email was successfully added to the list.");
+                    }
+                    else
+                    {
+                        alert("There was an error processing your request.");
+                    }
+
+                }
+            });
+        }
     }
 );
 
-
-
-
-function subscribeToNewsletter(){
-    new Ajax.Request('../test.html', {
-            method: 'get',
-            parameters: $('newsletter-validate-detail').serialize(true),
-            onSuccess: console.log(parameters)
-
-        }
-    )
-
-}
+function successFunc(response) {}
