d552fcfab31cb40be8c6ffebc265211fd3a447fe
Grant Wimmer <grant.wimmer@blueacorn.com>
2015-06-16 15:16:28 -0400

    Added controller to module.

---
+++ app/code/local/BlueAcorn/AjaxNewsletter/controllers/SubscribeController.php
@@ -0,0 +1,8 @@
+<?php
+class BlueAcorn_AjaxNewsletter_SubscribeController extends Mage_Core_Controller_Front_Action
+{
+    public function subscribeAction()
+    {
+        echo "Bonzai!";
+    }
+}
--- js/blueacorn/ajaxnewsletter.js
+++ js/blueacorn/ajaxnewsletter.js
@@ -6,7 +6,7 @@ Validation.prototype.onSubmit = Validation.prototype.onSubmit.wrap
 (
     function(callOriginal,ev) {
 
-        // The AJAX validation should only be done for the Newsletter Subscribe form
+        // The AJAX validation should only be done for the Newsletter Subscribe form. Otherwise, continue normal execution.
         if (this.form.id != 'newsletter-validate-detail')
             return callOriginal(ev);
 
@@ -19,29 +19,28 @@ Validation.prototype.onSubmit = Validation.prototype.onSubmit.wrap
         {
             // Stop the form from processing normally so that we may issue an Ajax call for email submission and simply display a message instead.
             Event.stop(ev);
-            console.log(this);
+
+            // Get the email address being submitted for inclusion in the Ajax call
             var email_addr = this.form[0].value;
 
+            // this.form.action = page that is originally called from standard form submission
+            // ((ROOT)/app/code/core/Mage/Newsletter/controllers/SubscriberController.php)
             new Ajax.Request(this.form.action, {
-                method: 'GET',
+                method:     'GET',
                 parameters: {addr: email_addr},
-                onSuccess: function(transport) {
-                    var request_status = transport.status;
-                    console.log(transport.status);
-                    if (transport.status === 200) // Request was processed successfully
-                    {
-                        alert("The email was successfully added to the list.");
-                    }
-                    else
-                    {
-                        alert("There was an error processing your request.");
-                    }
+                onSuccess:  function(transport) {
+                                                    var request_status = transport.status;
 
-
-                }
+                                                    if (transport.status === 200) // Request was processed successfully
+                                                    {
+                                                        alert("The email was successfully added to the list.");
+                                                    }
+                                                    else
+                                                    {
+                                                        alert("There was an error processing your request.");
+                                                    }
+                                                }
             });
         }
     }
 );
-
-function successFunc(response) {}
