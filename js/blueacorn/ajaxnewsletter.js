d941813b4081f7a42b2f0de9192c63d044d6d315
Grant Wimmer <grant.wimmer@blueacorn.com>
2015-06-17 18:05:07 -0400

    Cleaned up and refactored code for module

--- js/blueacorn/ajaxnewsletter.js
+++ js/blueacorn/ajaxnewsletter.js
@@ -1,43 +1,29 @@
-//this:         VarienForm('newsletter-validate-detail')
-//callOriginal: Original function Validation.prototype.onSubmit
-//ev:           The form submission event
+document.observe("dom:loaded", function()
+{
+    newsletterSubscriberFormDetail.form.observe('submit', function(ev) {
 
-Validation.prototype.onSubmit = Validation.prototype.onSubmit.wrap
-(
-    function(callOriginal,ev) {
+        var this_form = newsletterSubscriberFormDetail;
 
-        //return callOriginal(ev); // Used to override Ajax call and process request normally (for testing)
+        var is_valid = this_form.validator.validate();
 
-        // The AJAX validation should only be done for the Newsletter Subscribe form. Otherwise, continue normal execution.
-        if (this.form.id != 'newsletter-validate-detail')
-            return callOriginal(ev);
+        if (is_valid) {
 
-        if (!this.validate())
-        {
-            // Continue executing the original function (and pass back the event itself) so the user will be notified of the validation error
-            return callOriginal(ev);
-        }
-        else
-        {
-            // Stop the form from processing normally so that we may issue an Ajax call for email submission and simply display a message instead.
             Event.stop(ev);
 
-            // Get the email address being submitted for inclusion in the Ajax call
-            var email_addr = this.form[0].value;
+            var email_addr = this_form.form[0].value;
 
-            // Set URL to call for Ajax request. The email will be sent, and the status message returned.
-            var actionUrl = baseurl + 'ajaxnewsletter/subscribe/subscribe';
+            var http = location.protocol;
+            var slashes = http.concat("//");
+            var actionUrl = slashes.concat(window.location.hostname) +  '/index.php/ajaxnewsletter/subscribe/subscribe';
 
             new Ajax.Request(actionUrl, {
-                method:     'GET',
+                method: 'GET',
                 parameters: {email: email_addr},
-                onSuccess:  function(transport) {
+                onSuccess: function (transport) {
 
                     alert(transport.responseText);
-
-                    var request_status = transport.status;
                 }
             });
         }
-    }
-);
+    });
+});
