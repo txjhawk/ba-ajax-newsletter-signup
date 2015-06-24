81771f4a8699f531b25dcb384f26f87bd35ae860
Grant Wimmer <grant.wimmer@blueacorn.com>
2015-06-24 10:01:00 -0400

    Testng Observer

--- app/code/local/BlueAcorn/AjaxNewsletter/Model/Observer.php
+++ app/code/local/BlueAcorn/AjaxNewsletter/Model/Observer.php
@@ -2,32 +2,28 @@
 
 class BlueAcorn_AjaxNewsletter_Model_Observer
 {
-    public function __construct()
-    {
-
-    }
-
     public function enableDisableAjaxNewsletter(Varien_Event_Observer $observer)
     {
         $this->_toggleModule('BlueAcorn_AjaxNewsletter');
-
-
     }
 
     protected function _toggleModule($moduleName)
     {
+        $moduleStatus   = Mage::getStoreConfig('ajaxnewsoptions/ajaxsubmit/enabled'); // Will be 1 or 0
+
+
         $nodePath       = "modules/$moduleName/active";
-        $moduleStatus   = Mage::getStoreConfig('ajaxnewsoptions/ajaxsubmit/enabled') ? 'true' : 'false';
+        $moduleStatusBool   = Mage::getStoreConfig('ajaxnewsoptions/ajaxsubmit/enabled') ? 'true' : 'false';
 
         // Set the module itself
         Mage::getConfig()->setNode($nodePath, $moduleStatus, true);
 
 
         // Disable its output as well (which was already loaded)
-        $outputPath = "advanced/modules_disable_output/$moduleName";
-        if (!Mage::getStoreConfig($outputPath)) {
-            Mage::app()->getStore()->setConfig($outputPath, true);
-        }
+//        $outputPath = "advanced/modules_disable_output/$moduleName";
+//        if (!Mage::getStoreConfig($outputPath)) {
+//            Mage::app()->getStore()->setConfig($outputPath, true);
+//        }
 
     }
 }
