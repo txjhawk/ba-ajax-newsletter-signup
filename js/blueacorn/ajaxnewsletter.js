b236035885c7552cd6430719ced29b3153cc17a8
Grant Wimmer <grant.wimmer@blueacorn.com>
2015-06-24 14:18:39 -0400

    Update config.xml; test new JS

--- js/blueacorn/ajaxnewsletter.js
+++ js/blueacorn/ajaxnewsletter.js
@@ -1,29 +1,14 @@
-document.observe("dom:loaded", function()
-{
-    newsletterSubscriberFormDetail.form.observe('submit', function(ev) {
+var AjaxSubscribe = Class.create(
+    initialize: function(url) {
+        this.useurl = url;
+    }
 
-        var this_form = newsletterSubscriberFormDetail;
+    subscribe: function() {
+        alert('The url is ' + this.useurl);
 
-        var is_valid = this_form.validator.validate();
+    }
 
-        if (is_valid) {
 
-            Event.stop(ev);
 
-            var email_addr = this_form.form[0].value;
 
-            var http        = location.protocol;
-            var slashes     = http.concat("//");
-            var actionUrl   = slashes.concat(window.location.hostname) +  '/index.php/ajaxnewsletter/subscribe/subscribe';
-
-            new Ajax.Request(actionUrl, {
-                method:     'GET',
-                parameters: {email: email_addr},
-                onSuccess:  function (transport) {
-
-                    alert(transport.responseText);
-                }
-            });
-        }
-    });
-});
+);
