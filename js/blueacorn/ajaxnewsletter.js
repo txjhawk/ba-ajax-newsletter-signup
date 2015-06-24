e03b1954664dc95f730f80ef8c2246df8c6c17eb
Grant Wimmer <grant.wimmer@blueacorn.com>
2015-06-24 14:59:40 -0400

    test new JS

--- js/blueacorn/ajaxnewsletter.js
+++ js/blueacorn/ajaxnewsletter.js
@@ -1,10 +1,16 @@
 var AjaxSubscribe = Class.create({
     initialize: function (url) {
         this.useurl = url;
+        alert(this.useurl);
     },
 
-    subscribe: function () {
-        alert('The url is ' + this.useurl);
+    validate: function () {
+
 
     }
 });
+
+document.observe("dom:loaded", function()
+{
+    var as = new AjaxSubscribe('fiddle');
+}
