c70ad6bfed5b08111aa03bd60ba22c3b096e7321
Grant Wimmer <grant.wimmer@blueacorn.com>
2015-06-25 13:50:48 -0400

    Update ajaxnewsletter.js

--- js/blueacorn/ajaxnewsletter.js
+++ js/blueacorn/ajaxnewsletter.js
@@ -1,28 +1,30 @@
 var AjaxSubscribe = Class.create({
     initialize: function(url) {
         this.useurl = url;
-        alert(this.useurl);
+
+        // Set this as the current class so that the observer can call the class's methods.
         var parent = this;
 
         newsletterSubscriberFormDetail.form.observe('submit', function(ev) {
             console.log('Button has been clicked.');
+            console.log(ev);
             parent.validate(ev);
         });
     },
 
     validate: function(ev){
         var thisForm = newsletterSubscriberFormDetail;
+        var isValid  = thisForm.validator.validate();
 
-        console.log(thisForm);
+        if (isValid) {  // The entered email is valid
 
-        var isValid     = thisForm.validator.validate();
-
-        console.log(isValid);
-
-        if (isValid) {
+            // Stop the form from calling the php file that processes the email so that the ajax request can handle it
             Event.stop(ev);
+
+            var email_addr = thisForm.form[0].value;
         }
     }
 });
 
+// Create the variable as which will be set as an instance of the AjaxSubscribe class
 var as;
