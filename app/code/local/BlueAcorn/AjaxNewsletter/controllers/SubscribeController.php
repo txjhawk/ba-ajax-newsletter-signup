e2c8a1b1427099e9709b6aed84ada6518bb14394
Grant Wimmer <grant.wimmer@blueacorn.com>
2015-06-29 10:49:55 -0400

    Testing on message insertion

--- app/code/local/BlueAcorn/AjaxNewsletter/controllers/SubscribeController.php
+++ app/code/local/BlueAcorn/AjaxNewsletter/controllers/SubscribeController.php
@@ -5,34 +5,15 @@ class BlueAcorn_AjaxNewsletter_SubscribeController extends Mage_Core_Controller_
     {
         $email = $_GET['email'];
 
-        $valid_email = (Zend_Validate::is($email, 'EmailAddress'));
-
-        if (!$valid_email)
-        {
-            $message = "The email '{$email}' is invalid. Please enter a valid email address.";
+        try{
+            Mage::throwException($this->__('Please enter a valid email address.'));
+        }
+        catch (Mage_Core_Exception $e) {
+            $message = $this->__('There was a problem with the subscription: %s', $e->getMessage()));
             $status = "error";
         }
-        else
-        {
-            $newsletter = Mage::getModel('newsletter/subscriber');
 
-            if ($newsletter->loadByEmail($email)->getId())  // Check if email is already in the subscriber table
-            {
-                $message = "The email '{$email}' is already subscribed to our newsletter.";
-                $status = "error";
-            }
-            else
-            {
-                try {
-                    $newsletter->subscribe($email);
-                    $message = "Thank you for subscribing.";
-                    $status = "success";
-                } catch (Exception $e){
-                    $message = $e->getMessage();
-                    $status = "error";
-                }
-            }
-        }
+
 
         echo json_encode(array('status' => $status, 'message' => '<ul><li>' . $message . '</li></ul>'));
     }
